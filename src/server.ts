ced66cb25a8747986edea852ea3784fc944253e8
Misteri <136891190+TkachevskiyOleg@users.noreply.github.com>
2025-09-14 23:06:30 +0300

    Add_Log_server

--- src/server.ts
+++ src/server.ts
@@ -12,7 +12,11 @@ if (!fs.existsSync(uploadDir)) {
   fs.mkdirSync(uploadDir);
 }
 
-app.listen(PORT, () => {});
+app.listen(PORT, () => {
+  console.log(`🚀 Сервер запущено на порту ${PORT}`);
+  console.log(`📚 API доступне за адресою: http://localhost:${PORT}`);
+  console.log(`📖 Swagger документація: http://localhost:${PORT}/api-docs`);
+});
 
 process.on('SIGINT', async () => {
   await prisma.$disconnect();
